acfc7f47a518ccd731867197cd42b70d7df16c54
savan2004 <savan2004@gmail.com>
2026-02-10 16:59:26 +0530

    Delete tests directory

--- tests/test_ai.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from unittest.mock import patch
-from bot_main import get_signal  # Import from bot_main.py
-
-class TestAI(unittest.TestCase):
-    @patch('google.genai.GenerativeModel.generate_content')
-    def test_get
