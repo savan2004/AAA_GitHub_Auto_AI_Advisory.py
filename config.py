54f4bdd539c5c2bf4ab381d4ca297300748a04a3
savan2004 <savan2004@gmail.com>
2026-03-11 12:52:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 # config.py
-# Constants for tiers and freshness
 
 TIER_LIMITS = {
     "free": 50,
     "paid": 200,
 }
 
-# History freshness threshold (seconds)
-FRESHNESS_SECONDS = 3600  # 1 hour
+FRESHNESS_SECONDS = 3600
+DEFAULT_MAX_TOKENS = 500
