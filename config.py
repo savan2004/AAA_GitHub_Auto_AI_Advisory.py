f2b3f3a3a8915b829701960c2b81066ffecef933
savan2004 <savan2004@gmail.com>
2026-03-11 15:46:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
-# Optional separate config.
-# If you prefer keeping env variables here instead of main code.
-import os
 
+# config.py
+
+TIER_LIMITS = {
+    "free": 50,
+    "paid": 200,
+}
+
+FRESHNESS_SECONDS = 3600
+DEFAULT_MAX_TOKENS = 500
